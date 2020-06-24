39675c6eb319a4004a39c63f25e70b0774ec9ad8
mellownightpirate <a@hasan.co>
2020-06-24 22:59:19 +0100

    installed and imported modules and components into app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PhotoCard from "./Components/Photocard";
 import "./App.css";
 
 function App() {
